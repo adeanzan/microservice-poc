4d1304147ac6b977e8ec49e2e5364afacebbee5d
Alex Dean <alex@zanconsulting.com>
2021-05-30 11:49:33 -0600

    Add zan/doctrine-bundle

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Zan\DoctrineBundle\ZanDoctrineBundle::class => ['all' => true],
 ];
