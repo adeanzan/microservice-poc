351c2b66b924e8a032c9a36fadd58721dda855c7
Alex Dean <alex@zanconsulting.com>
2021-05-31 10:03:52 -0600

    Remove zan/doctrine-bundle (being replaced by zan/doctrine-rest-bundle)

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -3,7 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Zan\DoctrineBundle\ZanDoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
